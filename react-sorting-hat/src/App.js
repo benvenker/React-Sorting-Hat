f50cb122a723dc758abae11964ca2e5f4932963a
Ben Venker <bvemails@gmail.com>
2020-03-22 11:33:30 -0700

    add routes and components for routes

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Questionnaire from "./components/Questionnaire";
+import Welcome from "./components/Welcome";
+import Result from "./components/Result";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={Welcome}></Route>
+      <Route exact path="/questionnaire" component={Questionnaire}></Route>
+      <Route path="/result" component={Result}></Route>
     </div>
   );
 }
