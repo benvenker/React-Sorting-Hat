7b75efe81d96fcf4b4f2a511314428236a0673d8
Ben Venker <bvemails@gmail.com>
2020-03-22 11:23:46 -0700

    add some data

---
+++ react-sorting-hat/houses.js
@@ -0,0 +1,34 @@
+export const houses = [
+  {
+    name: "Gryffindor",
+    values: ["bravery", "daring", "nerve", "chivalry"],
+    animal: "Lion",
+    colors: ["scarlet", "gold"],
+    crestUrl:
+      "https://vignette.wikia.nocookie.net/harrypotter/images/e/ee/Gryffindor_Crest-0.jpg/revision/latest?cb=20160729145529"
+  },
+  {
+    name: "Hufflepuff",
+    values: ["hard work", "dedication", "patience", "loyalty", "fair play"],
+    animal: "Badger",
+    colors: ["scarlet", "gold"],
+    crestUrl:
+      "https://vignette.wikia.nocookie.net/harrypotter/images/6/62/Download_%289%29.jpg/revision/latest?cb=20160729145653"
+  },
+  {
+    name: "Ravenclaw",
+    values: ["intelligence", "knowledge", "wit"],
+    animal: "Eagle",
+    colors: ["scarlet", "gold"],
+    crestUrl:
+      "https://vignette.wikia.nocookie.net/harrypotter/images/2/28/Download_%284%29.jpg/revision/latest?cb=20160729145945"
+  },
+  {
+    name: "Slytherin",
+    values: ["ambition", "cunning", "resourcefulness"],
+    animal: "Serpent",
+    colors: ["emerald green", "silver"],
+    crestUrl:
+      "https://vignette.wikia.nocookie.net/harrypotter/images/7/72/Ccd53034-c798-452a-8032-f28e06250d8f.jpg/revision/latest?cb=20160729150848"
+  }
+];
